7f8288f678059e617984b9395e8c58bb449e5150
JowettC <57623021+JowettC@users.noreply.github.com>
2020-12-06 10:49:23 +0800

    file structure

---
+++ connection.js
@@ -0,0 +1,22 @@
+
+const mysql = require('mysql');
+
+var mysqlConnection = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"",
+    database: "Edureka",
+    multipleStatements : true
+})
+
+mysqlConnection.connect((err)=>{
+    if(!err){
+        console.log("connected")
+    }
+    else{
+        console.log("connection failed")
+    }
+
+});
+
+module.exports = mysqlConnection;
--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 const mysql = require('mysql');
 const express = require('express');
 var bodyParser = require('body-parser')
-const app = express();
 
+const PeopleRoutes = require("./routes/people")
+const mysqlConnection = require("./connection")
+
+
+const app = express();
 app.use(bodyParser.json());
 
-var mysqlConnection = mysql.createConnection({
-    host: "localhost",
-    user:"root",
-    password:"",
-    database: "Edureka",
-    multipleStatements : true
-})
+app.use("/people",PeopleRoutes);
 
-mysqlConnection.connect((err)=>{
-    if(!err){
-        console.log("connected")
-    }
-    else{
-        console.log("connection failed")
-    }
 
-})
 app.listen(3000);
